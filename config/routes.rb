373fdb7050773d363f270331f58291cac1ea9ad6
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-06-03 14:33:38 -0300

    add user athentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :purchase_order_details
-  resources :users
   resources :clients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
