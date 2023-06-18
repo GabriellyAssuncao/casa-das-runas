6aea50b7cfed450596d91d6246e350e9b8c2da1d
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-06-17 22:27:31 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :purchase_order_details
   resources :clients
+
+  resources :products
+  resources :purchase_orders
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
